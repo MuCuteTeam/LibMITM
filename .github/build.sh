7cc822b0f5df9d35c2301938e4cd0cfc9d8f0214
SuMuCheng <64734217+CaiMuCheng@users.noreply.github.com>
2025-02-03 21:23:01 +0800

    Update build.sh

--- .github/build.sh
+++ .github/build.sh
@@ -3,5 +3,7 @@
 source .github/env.sh
 
 go get -v -d
+go install -v golang.org/x/mobile/cmd/gomobile@v0.0.0-20221110043201-43a038452099
+go install -v golang.org/x/mobile/cmd/gobind@v0.0.0-20221110043201-43a038452099
 gomobile init
 gomobile bind -v -androidapi 28 -ldflags='-s -w' ./
