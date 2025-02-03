3c3a2b86553ebf66f4a8c2b468d3fe8554f656f2
SuMuCheng <64734217+CaiMuCheng@users.noreply.github.com>
2025-02-03 21:16:36 +0800

    Update build.sh

--- .github/build.sh
+++ .github/build.sh
@@ -3,10 +3,8 @@
 source .github/env.sh
 
 go get -v -d
-go install -v github.com/sagernet/gomobile/cmd/gomobile@v0.0.0-20221130124640-349ebaa752ca
 gomobile init
-go install -v github.com/sagernet/gomobile/cmd/gobind@v0.0.0-20221130124640-349ebaa752ca
 
 BUILD="../libmitm"
 
-gomobile bind -v -cache $(realpath $BUILD) -androidapi 28 -trimpath -tags='disable_debug' -ldflags='-s -w -buildid=' . || exit 1
+gomobile bind -v -androidapi 28 -ldflags='-s -w' ./
